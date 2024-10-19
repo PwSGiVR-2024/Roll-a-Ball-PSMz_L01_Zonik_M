bb8dd21f4a2ec24e8607f242244f8f785ce6b9fc
OGzonio <maciej.zonik1@gmail.com>
2024-10-19 13:10:17 +0200

    Movement mechanics

---
+++ Assets/movementController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class movementController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            print("siema cos sie dzieje");
+            m_Rigidbody.AddForce(Vector3.forward * m_Thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            print("siema cos sie dzieje");
+            m_Rigidbody.AddForce(-Vector3.forward * m_Thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            print("siema cos sie dzieje");
+            m_Rigidbody.AddForce(Vector3.left * m_Thrust);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            print("siema cos sie dzieje");
+            m_Rigidbody.AddForce(Vector3.right * m_Thrust);
+        }
+    }
+}
